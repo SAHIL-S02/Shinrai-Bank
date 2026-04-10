e179a8915030bb4971bfd628dad6cdbcf14a268c
SK SAHIL UDDIN <151388592+Akuma-002@users.noreply.github.com>
2026-04-10 08:06:51 +0530

    Update SideBar

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -3,28 +3,27 @@ import { useContext } from 'react'
 import { SideBarContextInfo } from '@/contexts/SideBarContext'
 import { useEffect } from "react";
 
-useEffect(() => {
-    console.log(sideBar);
-}, [sideBar]);
-
 const SideBar = () => {
     const {sideBar, setSideBar} = useContext(SideBarContextInfo);
+    useEffect(() => {
+    console.log(sideBar);
+    }, [sideBar]);
     return (
     <div className='sideBar w-16 lg:min-h-[800px] lg:max-h-[900px] bg-red flex flex-col bg-[#FFFFFF]'>
         <div className='upperSide'>
-            <div className={`dashboardIconDiv shadow-lg   mt-5 mb-5 ${sideBar == "dashboard"? "active" : "de-ative"}`} onClick={() => setSideBar("dashboard")}>
+            <div className={`dashboardIconDiv drop-shadow-lg   mt-5 mb-5 ${sideBar == "dashboard"? "active" : "de-active"}`} onClick={() => setSideBar("dashboard")}>
                 <img className='rounded-md' src="/icons/dashboard.png" alt="Dashboard" />
             </div>
-            <div className={`walletIconDiv shadow-lg  mt-5 mb-5 ${sideBar == "wallet"? "active" : "de-ative"}`} onClick={() => {setSideBar("wallet"); console.log(sideBar)}}>
+            <div className={`walletIconDiv drop-shadow-lg  mt-5 mb-5 ${sideBar == "wallet"? "active" : "de-active"}`} onClick={() => {setSideBar("wallet")}}>
                 <img className='rounded-md' src="/icons/wallet.png" alt="Wallet" />
             </div>
-            <div className='historyIconDiv shadow-lg  mt-5 mb-5 de-active'>
-                <img className='rounded-md' src="/icons/history.png" alt="History" onClick={setSideBar("dashboard")}/>
+            <div className={`historyIconDiv drop-shadow-lg  mt-5 mb-5 ${sideBar == "history"? "active" : "de-active"}`} onClick={() => {setSideBar("history")}}>
+                <img className='rounded-md' src="/icons/history.png" alt="History" />
             </div>
-            <div className='loanIconDiv shadow-lg w-[1.8rem] mt-5 mb-5 de-active' onClick={setSideBar("dashboard")}>
+            <div className={`loanIconDiv drop-shadow-lg w-[1.8rem] mt-5 mb-5 ${sideBar == "loan"? "active" : "de-active"}`} onClick={() => {setSideBar("loan")}}>
                 <img className='rounded-md' src="/icons/loan.png" alt="Loan" />
             </div>
-            <div className='investmentIconDiv shadow-lg  mt-5 mb-5 de-active' onClick={setSideBar("dashboard")}>
+            <div className={`investmentIconDiv drop-shadow-lg  mt-5 mb-5 ${sideBar == "investment"? "active" : "de-active"}`} onClick={() => {setSideBar("investment")}}>
                 <img className='rounded-md' src="/icons/investment.png" alt="Investment" />
             </div>
         </div>
