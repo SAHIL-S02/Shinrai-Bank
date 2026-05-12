6fa422f081329b3b6c32b74b72175927896871cb
SK SAHIL UDDIN <sksahilu735@gmail.com>
2026-05-12 19:44:28 +0530

    Update

--- frontend/src/services/supabaseClient.js
+++ frontend/src/services/supabaseClient.js
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-let a = 0
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
